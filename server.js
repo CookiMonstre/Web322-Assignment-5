765a844a7c3d8fe8a6d49537d98f5c0774f3e757
Jacob Van Halteren <jvan-halteren@sy-t3078-36.local>
2018-09-19 10:23:40 -0400

    N/A

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+// setup a 'route' to listen on the default url path (http://localhost)
+app.get("/", function(req,res){
+   res.sendfile(views/home.html);
+});
+
+// setup another route to listen on /about
+app.get("/about", function(req,res){
+   res.sendfile(views/about.htm);
+});
+
+// i have no clue what this does ..
+app.use(express.static('public'));
+//--------------------------------
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT, onHttpStart);
