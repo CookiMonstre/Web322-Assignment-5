397fa3a4bfd31a0b815279c920d1f5a30bfb4cd6
Jacob Van Halteren <jvan-halteren@sy-t3078-36.local>
2018-10-03 10:15:00 -0400

    yes

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-
+/*********************************************************************************
+* WEB322 – Assignment 02
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+* of this assignment has been copied manually or electronically from any other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Jacob Van Halteren Student ID: 111769170 Date: ________________
+*
+* Online (Heroku) Link: ________________________________________________________
+*
+********************************************************************************/
 
 var express = require("express");
 var path = require("path");
