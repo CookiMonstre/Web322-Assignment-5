494d7eb361aac41a93ece10d5e1918a8c2614a7e
JVAN-HALTEREN <JVAN-HALTEREN@myseneca.ca>
2018-11-13 12:42:13 -0500

    s

--- server.js
+++ server.js
@@ -31,20 +31,20 @@ app.engine(".hbs", exphbs({
     extname: ".hbs",
     defaultLayout: "main",
     helpers: {
-        equal: (lvalue, rvalue, options) => {
-            if (arguments.length < 3)
-                throw new Error("Handlebars Helper equal needs 2 parameters");
-            if (lvalue != rvalue) {
-                return options.inverse(this);
-            } else {
-                return options.fn(this);
-            }
-				},
 				navLink: function(url, options){
 					return '<li' + 
 					((url == app.locals.activeRoute) ? ' class="active" ' : '') + 
 					'><a href="' + url + '">' + options.fn(this) + '</a></li>';
-				}
+				},
+				equal: function (lvalue, rvalue, options) {
+					if (arguments.length < 3)
+							throw new Error("Handlebars Helper equal needs 2 parameters");
+					if (lvalue != rvalue) {
+							return options.inverse(this);
+					} else {
+							return options.fn(this);
+					}
+			}			
     }
 }));
 app.set("view engine", ".hbs");
