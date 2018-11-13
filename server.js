24fa7fbe1323539df05133313b16943e7eafa07e
JVAN-HALTEREN <JVAN-HALTEREN@myseneca.ca>
2018-11-13 13:45:52 -0500

    s

--- server.js
+++ server.js
@@ -105,27 +105,27 @@ app.get("/images", (req,res) => {
 app.get("/employees", (req, res) => {
     if (req.query.status) {
         data.getEmployeesByStatus(req.query.status).then((data) => {
-            res.json(data);
+					res.render("employees", {employees: data});
         }).catch((err) => {
-            res.json({ message: "no results" });
+            res.render({ message: "no results" });
         });
     } else if (req.query.department) {
         data.getEmployeesByDepartment(req.query.department).then((data) => {
-            res.json(data);
+					res.render("employees", {employees: data});
         }).catch((err) => {
-            res.json({ message: "no results" });
+            res.render({ message: "no results" });
         });
     } else if (req.query.manager) {
         data.getEmployeesByManager(req.query.manager).then((data) => {
-            res.json(data);
+					res.render("employees", {employees: data});
         }).catch((err) => {
-            res.json({ message: "no results" });
+            res.render({ message: "no results" });
         });
     } else {
         data.getAllEmployees().then((data) => {
-            res.json(data);
+					res.render("employees", {employees: data});
         }).catch((err) => {
-            res.json({ message: "no results" });
+            res.render({ message: "no results" });
         });
     }
 });
